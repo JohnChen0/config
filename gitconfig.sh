fe5bfc67234e5d10056d036db09d5b60ab5ec14a
John Chen <johnchen@chromium.org>
2022-06-08 13:15:15 -0700

    Add gitconfig.sh

---
+++ gitconfig.sh
@@ -0,0 +1,8 @@
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.ds "diff --name-status --find-renames"
+git config --global alias.l1 "log --oneline --decorate --all --graph"
+git config --global alias.pf "pull --ff-only"
+git config --global alias.ss "status -sb"
+git config --global alias.st status
