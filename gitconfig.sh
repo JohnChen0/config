88bbff02222304d64c28b732dc06b36f1c8e1a41
John Chen <john_chen@outlook.com>
2025-06-07 22:23:06 -0700

    Some adjustments

--- gitconfig.sh
+++ gitconfig.sh
@@ -5,7 +5,10 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.ds "diff --name-status --find-renames"
-git config --global alias.l1 "log --oneline --decorate --all --graph"
+git config --global alias.l1 "log --oneline --all --graph"
 git config --global alias.pf "pull --ff-only"
 git config --global alias.ss "status -sb"
 git config --global alias.st status
+
+git config --global gc.auto 0
+git config --global push.default current
